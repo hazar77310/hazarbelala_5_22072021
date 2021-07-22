3874ee9da179f7566d8a910d0c572459c776c275
hazar belala <hazar.belala@gmail.com>
2021-07-22 22:38:01 +0200

    essaie du js pour produit

--- produit.js
+++ produit.js
@@ -1,1 +1,21 @@
-fetch("http://localhost:3000/api/teddies/)
+let product_sheet = fetch('http://localhost:3000/api/teddies')
+  .then(function(response){
+    return response.json()
+  }).then(function(data){
+     // appel de ta fonction d'affichage
+     afficheDatas(data);
+});
+
+function afficheDatas(data) {
+  data.map(function(produit, index) {
+   document
+      .querySelector(".peluche")
+      .insertAdjacentHTML("beforebegin","<div id='"+produit._id+"'>"
+      +"<div>image :<img src='"+produit.imageUrl+"' alt='image_produit' /></div>"
+      +"<div>nom :"+produit.name+"</div>"
+      +"<div>description :"+produit.description+"</div>"
+      +"<div>couleurs :"+produit.colors+"</div>"
+      +"<div>prix :"+produit.price+"</div>"
+      +"</div>");
+  });
+
