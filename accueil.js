c09688587ed8bf42974b5f36255ef08ce2087ab0
hazar belala <hazar.belala@gmail.com>
2021-08-01 17:49:27 +0200

    essaie js page d'accueil

--- accueil.js
+++ accueil.js
@@ -1,14 +1,7 @@
+let teddies 
+let $produit = document.querySelector('#produit');
 
-(async function() {
-  const articles = await getArticles()
-  
-  for (article of articles) {
-    displayArticle(article)
-  }
-})()
-
-function getArticles() {
-  return fetch ("http://localhost:3000/api/teddies")
+  fetch ("http://localhost:3000/api/teddies")
     .then(function(response){ //utilisation de then pour récupérer une promesse qui va nous donner une réponse 
       return response.json() //type de format réponse en json
     })
@@ -18,15 +11,24 @@ function getArticles() {
     .catch(function(error) {
       alert("Une erreur est survenue. Nous allons corriger le problème très prochainement")
     })
-}
 
-function displayArticle(article) {
-  const templateElt = document.getElementById("produit")
-  const cloneElt = document.importNode(templateElt.content, true)
 
-  cloneElt.getElementById('image').src = produit.imageUrl
-  cloneElt.getElementById('nom').textContent = produit.name
-  cloneElt.getElementById('prix').textContent = `${produit.price / 100}.00 €`
-  cloneElt.getElementById('lien').href = `/produit.html?id=${produit._id}`
+    //Appel URL
+const params = document.location;
+console.log("params", params)
+// const id = params.get('id'); //Obtiens l'id du produit
+
+const main = () => {
+  $produit.innerHTML +=
+    `<section class="main" id="main">
+      <h1 class="titre">Nos produits</h1>
+      <div id="produit">
+        <article class="boxprod">
+          <a href="produit.html?id=${article.id}" id="lien" class="lien"><img alt="image" id="image" src="" url(${article.imageUrl}></a>
+          <h2 id="nom" class="nom">${article.name}</h2>
+          <p id="prix" class="prix">${article.price}</p>
+        </article>
+      </div>
+    </section>`
 
 }
