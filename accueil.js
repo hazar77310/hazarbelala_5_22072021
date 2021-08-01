39c3a98aad5287cdf8f30d1d656d88850e724daa
hazar belala <hazar.belala@gmail.com>
2021-08-01 17:05:22 +0200

    essaie js

--- accueil.js
+++ accueil.js
@@ -1,19 +1,32 @@
-let produit = fetch('http://localhost:3000/api/teddies')
-  .then(function(response){
-    return response.json()
-  }).then(function(data){
-     // appel de ta fonction d'affichage
-     afficheDatas(data);
-});
 
-function afficheDatas(data) {
-  data.map(function(produit, index) {
-   document
-      .querySelector(".produit")
-      .insertAdjacentHTML("beforebegin","<div id='"+produit._id+"'>"
-      +"<div>image :<img src='"+produit.imageUrl+"' alt='image_produit' /></div>"
-      +"<div>nom :"+produit.name+"</div>"
-      +"<div>prix :"+produit.price+"</div>"
-      +"</div>");
-  });
+(async function() {
+  const articles = await getArticles()
+  
+  for (article of articles) {
+    displayArticle(article)
+  }
+})()
 
+function getArticles() {
+  return fetch ("http://localhost:3000/api/teddies")
+    .then(function(response){ //utilisation de then pour récupérer une promesse qui va nous donner une réponse 
+      return response.json() //type de format réponse en json
+    })
+    .then(function(articles) {
+      return articles
+    })
+    .catch(function(error) {
+      alert("Une erreur est survenue. Nous allons corriger le problème très prochainement")
+    })
+}
+
+function displayArticle(article) {
+  const templateElt = document.getElementById("produit")
+  const cloneElt = document.importNode(templateElt.content, true)
+
+  cloneElt.getElementById('image').src = produit.imageUrl
+  cloneElt.getElementById('nom').textContent = produit.name
+  cloneElt.getElementById('prix').textContent = `${produit.price / 100}.00 €`
+  cloneElt.getElementById('lien').href = `/produit.html?id=${produit._id}`
+
+}
--- produit.js
+++ produit.js
@@ -1,21 +1,28 @@
-let peluche = fetch('http://localhost:3000/api/teddies')
-  .then(function(response){
-    return response.json()
-  }).then(function(data){
-     // appel de ta fonction d'affichage
-     afficheDatas(data);
-});
+fetch('http://localhost:3000/api/teddies') //url qui permet de recueillir des données
+  .then(function(response){ //utilisation de then pour récupérer une promesse qui va nous donner une réponse 
+    return response.json() //type de format réponse en json
+  })
+  .then(function(data) { //récupération d'une autre promesse pour obtenir des données 
+    var el = document.querySelector(".produitours");//selectionne le premier élément
+    el.innerText=JSON.stringify(data); //change le texte qui est dans une balise html
+    console.log(el) //s'affiche sur la console du site
+  } );
 
-function afficheDatas(data) {
-  data.map(function(produit, index) {
-   document
-      .querySelector(".peluche")
-      .insertAdjacentHTML("beforebegin","<div id='"+produit._id+"'>"
-      +"<div>image :<img src='"+produit.imageUrl+"' alt='image_produit' /></div>"
-      +"<div>nom :"+produit.name+"</div>"
-      +"<div>description :"+produit.description+"</div>"
-      +"<div>couleurs :"+produit.colors+"</div>"
-      +"<div>prix :"+produit.price+"</div>"
-      +"</div>");
-  });
+
+let params = new URL(document.location).searchParams;
+let id = params.get("id");
+function getArticles () {
+  fetch('http://localhost:3000/api/teddies/${id}') //url qui permet de recueillir des données
+    .then(function(response){ //utilisation de then pour récupérer une promesse qui va nous donner une réponse 
+      return response.json() //type de format réponse en json
+    })
+    .then(function(resultatAPI) { //on place les donnees sur la page  
+      article = resultatAPI;
+      nom.innerHTML = article.name;
+      image.src = article.imageUrl;
+      description.innerText = article.description;
+    })
+}
+
+
 
