58eeea69170e482a5c29ee16930460e1444b6414
hazar belala <hazar.belala@gmail.com>
2021-09-10 17:50:27 +0200

    essaie js pour page d'accueil et fiche produit

--- accueil.js
+++ accueil.js
@@ -5,28 +5,30 @@ let $produit = document.querySelector('#produit');
 
 //appel de l'API
 
-fetch ("http://localhost:3000/api/teddies")
-  .then(function(response){ //utilisation de then pour récupérer une promesse qui va nous donner une réponse 
-    return response.json() //type de format réponse en json
+fetch("http://localhost:3000/api/teddies")
+    .then(function (response) {
+        //utilisation de then pour récupérer une promesse qui va nous donner une réponse
+        return response.json() //type de format réponse en json
     })
-  .then(function(articles) {
-    articles.forEach(elem) => {
-      main(elem)}
+    .then(function (articles) {
+        articles.forEach((elem) => main(elem))
     })
-  .catch(function(error) {
-    alert("Une erreur est survenue. Nous allons corriger le problème très prochainement")
-})
+    .catch(function (error) {
+        alert(
+            "Une erreur est survenue. Nous allons corriger le problème très prochainement"
+        )
+    })
 
 
 //Appel URL
 const params = document.location;
 console.log(params);
 
-function main = (teddie) => {
+function main(teddie) {
   let newDiv =document.createElement("div")
   newDiv.innerHTML =
     `<section class="main" id="main">
-      <h1 class="titre">Nos produits</h1>
+      <h1 class="titre"></h1>
       <div id="produit">
         <article class="boxprod">
           <a href="produit.html?id=${teddie._id}" id="lien" class="lien"><img alt="image" id="image" src="${teddie.imageUrl}"></a>
--- produit.js
+++ produit.js
@@ -1,3 +1,6 @@
+let teddies 
+let $produit = document.querySelector('#produit');
+
 fetch('http://localhost:3000/api/teddies') //url qui permet de recueillir des données
   .then(function(response){ //utilisation de then pour récupérer une promesse qui va nous donner une réponse 
     return response.json() //type de format réponse en json
@@ -8,20 +11,31 @@ fetch('http://localhost:3000/api/teddies') //url qui permet de recueillir des do
     console.log(el) //s'affiche sur la console du site
   } );
 
+//Appel URL
 
-let params = new URL(document.location).searchParams;
-let id = params.get("id");
-function getArticles () {
-  fetch('http://localhost:3000/api/teddies/${id}') //url qui permet de recueillir des données
-    .then(function(response){ //utilisation de then pour récupérer une promesse qui va nous donner une réponse 
-      return response.json() //type de format réponse en json
-    })
-    .then(function(resultatAPI) { //on place les donnees sur la page  
-      article = resultatAPI;
-      nom.innerHTML = article.name;
-      image.src = article.imageUrl;
-      description.innerText = article.description;
-    })
+const url_id = window.location.search;
+console.log (url_id);
+
+const urlSearchparams = new URLSearchParams(url_id);
+console.log(urlSearchparams);
+
+function main(teddie) {
+  let newDiv =document.createElement("div")
+  newDiv.innerHTML =
+    `<section class="main" id="main">
+      <h1 class="titre"></h1>
+      <div id="produit">
+        <article class="boxprod">
+          <a href="produit.html?id=${teddie._id}" id="lien" class="lien"><img alt="image" id="image" src="${teddie.imageUrl}"></a>
+          <h2 id="nom" class="nom">${teddie.name}</h2>
+          <div class="description">${teddie.description}</div>
+          <div class="couleurs">${teddie.colors}</div>
+          <p id="prix" class="prix">${teddie.price}</p>
+        </article>
+      </div>
+    </section>`
+    $produit.appendChild(newDiv)
+
 }
 
 
