4f7ec4ca511f9d0750e4bd3bdb58561804d8d08d
hazar belala <hazar.belala@gmail.com>
2021-09-16 15:58:13 +0200

    essaie js fiche produit

--- produit.js
+++ produit.js
@@ -1,29 +1,18 @@
-let teddies 
-let $produit = document.querySelector('#produit');
+const productId = new URL(location.href).searchParams.get("id");
+console.log(productId);
 
-fetch('http://localhost:3000/api/teddies') //url qui permet de recueillir des données
+fetch('http://localhost:3000/api/teddies/' + productId)  //url qui permet de recueillir des données
   .then(function(response){ //utilisation de then pour récupérer une promesse qui va nous donner une réponse 
     return response.json() //type de format réponse en json
   })
-  .then(function(data) { //récupération d'une autre promesse pour obtenir des données 
-    var el = document.querySelector(".produitours");//selectionne le premier élément
-    el.innerText=JSON.stringify(data); //change le texte qui est dans une balise html
-    console.log(el) //s'affiche sur la console du site
-  } );
+  .then((teddy) => { //récupération d'une autre promesse pour obtenir des données 
+    console.log(teddy) //s'affiche sur la console du site
 
-//Appel URL
 
-const url_id = window.location.search;
-console.log (url_id);
-
-const urlSearchparams = new URLSearchParams(url_id);
-console.log(urlSearchparams);
-
-function main(teddie) {
-  let newDiv =document.createElement("div")
-  newDiv.innerHTML =
-    `<section class="main" id="main">
-      <h1 class="titre"></h1>
+    function main(teddie) {
+    let newDiv =document.createElement("div")
+    newDiv.innerHTML =
+      `<section class="main" id="main">
       <div id="produit">
         <article class="boxprod">
           <a href="produit.html?id=${teddie._id}" id="lien" class="lien"><img alt="image" id="image" src="${teddie.imageUrl}"></a>
@@ -33,10 +22,21 @@ function main(teddie) {
           <p id="prix" class="prix">${teddie.price}</p>
         </article>
       </div>
-    </section>`
-    $produit.appendChild(newDiv)
+     </section>`
+      $produit.appendChild(newDiv)
+      console.log(newDiv)
 
 }
+    function couleur(colors) {
+      let divcolor = document.getElementByClassName("couleurs");
+      divcolor.innerText = document.getElementByClassName("couleurs", `<div class="couleurs">${teddie.colors}</div>`);
+      colors.forEach((elem) => (elem))
+    console.log(divcolor);
+    $couleurs.appendChild(newDiv)
+    }
+
+  } );
+
 
 
 
