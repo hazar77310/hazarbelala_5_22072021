be231e99e1f7d69581fcf5e40693434a972e4505
hazar belala <hazar.belala@gmail.com>
2021-09-16 23:00:06 +0200

    correction de la fonction main

--- produit.js
+++ produit.js
@@ -6,36 +6,12 @@ fetch('http://localhost:3000/api/teddies/' + productId)  //url qui permet de rec
     return response.json() //type de format réponse en json
   })
   .then((teddy) => { //récupération d'une autre promesse pour obtenir des données 
-    console.log(teddy) //s'affiche sur la console du site
+    main(teddy) // initie la fonction main
+  });
 
-
-    function main(teddie) {
-    let newDiv =document.createElement("div")
-    newDiv.innerHTML =
-      `<section class="main" id="main">
-      <div id="produit">
-        <article class="boxprod">
-          <a href="produit.html?id=${teddie._id}" id="lien" class="lien"><img alt="image" id="image" src="${teddie.imageUrl}"></a>
-          <h2 id="nom" class="nom">${teddie.name}</h2>
-          <div class="description">${teddie.description}</div>
-          <div class="couleurs">${teddie.colors}</div>
-          <p id="prix" class="prix">${teddie.price}</p>
-        </article>
-      </div>
-     </section>`
-      $produit.appendChild(newDiv)
-      console.log(newDiv)
-
+function main(teddie) {
+  document.querySelector("#nom") = teddie.name
 }
-    function couleur(colors) {
-      let divcolor = document.getElementByClassName("couleurs");
-      divcolor.innerText = document.getElementByClassName("couleurs", `<div class="couleurs">${teddie.colors}</div>`);
-      colors.forEach((elem) => (elem))
-    console.log(divcolor);
-    $couleurs.appendChild(newDiv)
-    }
-
-  } );
 
 
 
