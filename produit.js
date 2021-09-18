d0ef3d590f9ed62fb78884f5c78ac6ceb439c904
hazar belala <hazar.belala@gmail.com>
2021-09-18 17:46:50 +0200

    correction des parametres pour un probleme d'affichage de l'id

--- produit.js
+++ produit.js
@@ -1,13 +1,21 @@
-const productId = new URL(location.href).searchParams.get("id")
-console.log(productId);
+const prod = document.querySelector('#produit');
+let params = new URL(document.location).searchParams;
+let id = params.get("id");
+console.log(id);
 
-fetch('http://localhost:3000/api/teddies/' + productId)  //url qui permet de recueillir des données
+
+fetch('http://localhost:3000/api/teddies/')  //url qui permet de recueillir des données
   .then(function(response){ //utilisation de then pour récupérer une promesse qui va nous donner une réponse 
     return response.json() //type de format réponse en json
   })
   .then((teddy) => { //récupération d'une autre promesse pour obtenir des données 
     main(teddy) // initie la fonction main
-  });
+  })
+  .catch(function (error) {
+    alert(
+      "Une erreur est survenue. Nous allons corriger le problème très prochainement"
+    )
+  }) ; 
 
 
 function main(teddie) {
@@ -18,7 +26,6 @@ function main(teddie) {
   document.querySelector("#prix").innerText = teddie.price
 
 }
-
 
 
 
