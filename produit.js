d1aa63e7b49905c854cd9adef1d9007c0b4c627a
hazar belala <hazar.belala@gmail.com>
2021-09-21 18:34:35 +0200

    ajout du local storage

--- produit.js
+++ produit.js
@@ -31,7 +31,7 @@ function main(teddie) {
       option.innerText = teddie.colors [i]
       select.appendChild(option)
     }
-  document.querySelector("#prix").innerText = (teddie.price/100)
+  document.querySelector("#prix").innerText = (teddie.price)/ 100 + ",00" +"€"
 
 };
 
@@ -49,6 +49,54 @@ Quantitydiv.classList.add("quantity");
 let panierButton = document.createElement("button");
 panierButton.textContent = "Ajouter au panier";
 Quantitydiv.appendChild(panierButton);
-// Ajout de l'évènement "click" sur le bouton "moins"
-panierButton.addEventListener("click", function(){
-});
+
+// Déclaration de la variable ProduitLocalStorage. 
+//Son rôle est de retranscrire en javascript la valeur envoyée par "getItem("produit") en un objet réutilisable.
+let produitLocalStorage = JSON.parse(localStorage.getItem("#produit"));
+console.log(produitLocalStorage);
+
+// Création d'un objet qui contient les options sélectionnées pour les envoyer dans le local storage
+function objet (teddie) {
+  let optionsProduit = {
+    nom: teddie.name,
+    idProduit: teddie._id,
+    prix: teddie.price,
+    quantity: document.getElementsByTagName("input type : number")
+  }
+};
+
+//********************LOCAL STORAGE******************//
+//************Stocker la récupération des valeurs du formulaire dans le local storage
+
+//Déclaration de lq variable "produitEnregistreDansLocalStorage" dans laquelle on met les keys qui sont dans le local storage
+let produitEnregistreDansLocalStorage = JSON.parse(localStorage.getItem("#produit"));
+//***JSON.parse c'est pour convertir les données au format JSON qui sont dans le local storage en objet JavaScript
+console.log(produitEnregistreDansLocalStorage);
+
+//fenêtre pop up
+const popupConfirmation = () => {
+  if (window.confirm (`${teddie.name} option: ${teddie.colors} a bien été ajouté au panier . Consultez le panier OK ou revenir à l'accueil ANNULER`)) {
+    window.location.href = "panier.html";
+  }else {
+    window.location.href = "index.html" ;
+  }
+}
+
+//S'il y a déja des produit enregistrés dans le local storage
+if (produitEnregistreDansLocalStorage) {
+    produitEnregistreDansLocalStorage.push(objet);
+    localStorage.setItem("produit" , JSON.stringify(produitEnregistreDansLocalStorage));
+    console.log(produitEnregistreDansLocalStorage);
+
+//S'il n'y a pas de produit enregistrés dans le local storage
+}else {
+  produitEnregistreDansLocalStorage = [];
+  produitEnregistreDansLocalStorage.push(objet);
+  localStorage.setItem("produit" , JSON.stringify(produitEnregistreDansLocalStorage));
+  console.log(produitEnregistreDansLocalStorage); 
+   
+}
+
+
+
+
